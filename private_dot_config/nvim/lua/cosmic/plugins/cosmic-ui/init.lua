1f8d5ce99234ff4d8dd56ee29074b395fced4555
askeko <askeklitgaard@gmail.com>
2022-01-23 18:13:00 +0100

    Delete cosmic vim

--- private_dot_config/nvim/lua/cosmic/plugins/cosmic-ui/init.lua
+++
@@ -1,8 +0,0 @@
-local config = require('cosmic.config')
-local utils = require('cosmic.utils')
-
-local defaults = {
-  border_style = 'rounded',
-}
-
-require('cosmic-ui').setup(utils.merge(defaults, config.cosmic_ui or {}))
