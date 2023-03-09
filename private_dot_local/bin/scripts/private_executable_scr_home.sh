44fce12182919497893ee8abcaeed5c057bab453
askeko <askeklitgaard@gmail.com>
2023-03-09 08:48:42 +0100

    home monitor setup script

--- private_dot_local/bin/scripts/private_executable_scr_home.sh
+++ private_dot_local/bin/scripts/private_executable_scr_home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr --output eDP --mode 1920x1080 --pos 5120x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-4 --off
 setbg ~/.local/share/bg
-xinput --map-to-output 'Wacom HID 52AE Pen stylus' eDP
-xinput --map-to-output 'Wacom HID 52AE Pen eraser' eDP
-xinput --map-to-output 'Wacom HID 52AE Finger touch' eDP
+xinput --map-to-output 'Wacom HID 52AE Pen Pen (0x80658cd9)' eDP
+xinput --map-to-output 'Wacom HID 52AE Pen Eraser (0x80658cd9)' eDP
+xinput --map-to-output 'Wacom HID 52AE Finger' eDP
