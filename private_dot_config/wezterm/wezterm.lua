24a29d48463c9fd62225909b60680f954bd07d08
askeko <askeklitgaard@gmail.com>
2023-08-09 12:43:00 +0200

    change to tokyonight-storm

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,24 +7,27 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Tokyo Night"
-config.font = wezterm.font (
-  "FiraCode Nerd Font Mono", {weight="Regular", italic = true, stretch="Normal", style="Normal"}
-)
-config.font = wezterm.font_with_fallback {
-  "FiraCode Nerd Font Mono",
-  "JetBrains Mono",
-}
+config.color_scheme = "tokyonight_storm"
+config.font = wezterm.font("FiraCode Nerd Font Mono", {
+	weight = "Regular",
+	italic = true,
+	stretch = "Normal",
+	style = "Normal",
+})
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font Mono",
+	"JetBrains Mono",
+})
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 25,
-  right = 25,
-  top = 25,
-  bottom = 25,
+	left = 25,
+	right = 25,
+	top = 25,
+	bottom = 25,
 }
 config.window_background_opacity = 0.85
 
