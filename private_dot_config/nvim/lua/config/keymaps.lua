28dedc5d6f69bad02798f50a1d448f7e41cf5e0b
askeko <askeklitgaard@gmail.com>
2023-09-09 23:51:54 +0200

    add nvim config, small hyprland fixes

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,42 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Brings up the file explorer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+--
 -- Visual line moves lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Cursor stays when moving lines up
-vim.keymap.set("n", "J", "mzJ`z")
--- Cursor in the middle when half page jumping
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- Search terms stays in the middle
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- Keeps clipboard when pasting over a word
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- Yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
--- Delete to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>d", [["+d]])
-
--- Format file
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
--- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
--- Replace word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
